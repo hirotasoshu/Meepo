cabb38fe1c7d073d3f1bfef1a8cd642584ea5720
Maksim Zayakin <rasetsu@list.ru>
2019-11-11 19:38:43 +0500

    removed dbscan; refactoring

--- JAD.py
+++ JAD.py
@@ -1,9 +1,8 @@
 import numpy as np
 from sklearn.neighbors import KDTree
-from sklearn.cluster import DBSCAN
 
 
-class KDTreeLabeling:
+class JAD:
 
     def __init__(self, r, bound):
         self.r = r
@@ -23,15 +22,14 @@ class KDTreeLabeling:
     def _check_space_and_classes(space, classes):
         if not np.all(np.isfinite(space)) and space.size:
             raise ValueError("The space is empty or contains NaN's")
-        if not np.all(np.isfinite(classes)) and classes.size:
+        elif not np.all(np.isfinite(classes)) and classes.size:
             raise ValueError("The classes is empty or contains NaN's")
-        if len(space) != len(classes):
+        elif len(space) != len(classes):
             raise ValueError("Space size must be equal to classes size")
 
     def _get_labels(self, tree, space, classes):
         labels = np.array([-1] * len(classes), dtype=int)
         labels = self._get_mixed_points(classes, labels, space, tree)
-        labels = self._get_labels_for_mix_points(labels, space)
         return labels
 
     def _get_mixed_points(self, classes, labels, space, tree):
@@ -44,22 +42,10 @@ class KDTreeLabeling:
                 labels[idx] = classes[idx]
         return labels
 
-    @staticmethod
-    def _get_labels_for_mix_points(labels, space):
-        mixed_points = space[labels == -1]
-        if mixed_points.size:
-            mixed_points_labels = DBSCAN(eps=3, min_samples=2).fit_predict(mixed_points)
-            mixed_points_labels = np.array([(label + 1)*(-1) for label in mixed_points_labels])
-            j = 0
-            for i in range(len(labels)):
-                if labels[i] == -1:
-                    labels[i] = mixed_points_labels[j]
-                    j += 1
-        return labels
-
     def fit_predict(self, space, classes):
         self._check_space_and_classes(space, classes)
         tree = KDTree(space)
         labels = self._get_labels(tree, space, classes)
         return labels
+
 
--- JAD_tests.py
+++ JAD_tests.py
@@ -1,48 +1,48 @@
 import pytest
 import numpy as np
-from kdtree_labeling import KDTreeLabeling
+from JAD import JAD
 
 
 def test_fail_on_negative_r():
     with pytest.raises(ValueError):
-        KDTreeLabeling(r=-3, bound=2)
+        JAD(r=-3, bound=2)
 
 
 def test_fail_on_negative_bound():
     with pytest.raises(ValueError):
-        KDTreeLabeling(r=3, bound=-2)
+        JAD(r=3, bound=-2)
 
 
 def test_fail_on_empty_space():
-    labeling = KDTreeLabeling(r=1, bound=2)
+    jad = JAD(r=1, bound=2)
     space = np.array([])
     classes = np.array([])
     with pytest.raises(ValueError):
-        labeling.fit_predict(space, classes)
+        jad.fit_predict(space, classes)
 
 
 def test_fail_on_space_with_nans():
-    labeling = KDTreeLabeling(r=1, bound=2)
+    jad = JAD(r=1, bound=2)
     space = np.array([[np.inf, np.nan], [5, 4]])
     classes = np.array([0, 1])
     with pytest.raises(ValueError):
-        labeling.fit_predict(space, classes)
+        jad.fit_predict(space, classes)
 
 
 def test_fail_on_empty_classes():
-    labeling = KDTreeLabeling(r=1, bound=2)
+    jad = JAD(r=1, bound=2)
     space = np.array([[5, 5]])
     classes = np.array([])
     with pytest.raises(ValueError):
-        labeling.fit_predict(space, classes)
+        jad.fit_predict(space, classes)
 
 
 def test_fail_on_classes_with_nans():
-    labeling = KDTreeLabeling(r=1, bound=2)
+    jad = JAD(r=1, bound=2)
     space = np.array([5, 3])
     classes = np.array([np.nan])
     with pytest.raises(ValueError):
-        labeling.fit_predict(space, classes)
+        jad.fit_predict(space, classes)
 
 
 @pytest.mark.parametrize("space, classes, expected_labels", [
@@ -50,11 +50,13 @@ def test_fail_on_classes_with_nans():
     (np.array([[5, 5], [4, 4]]), np.array([0, 0]), [0, 0]),
     (np.array([[5, 5], [4, 4], [10, 10], [20, 20]]), np.array([0, 0, 1, 1]), [0, 0, 1, 1]),
     (np.array([[5, 5], [4, 4], [10, 10], [15, 15], [150, 200], [350, 150]]),
-     np.array([0, 0, 1, 1, 2, 2]), [0, 0, 1, 1, 2, 2])
-], ids=['one_point', 'two_points_from_one_class', 'four_points_from_two_classes', 'six_points_from_three_classes'])
+     np.array([0, 0, 1, 1, 2, 2]), [0, 0, 1, 1, 2, 2]),
+    (np.array([5, 10]).reshape(-1, 1), np.array([0, 1]), [0, 1])
+], ids=['one_point', 'two_points_from_one_class', 'four_points_from_two_classes', 'six_points_from_three_classes',
+        '1d_date_from_two_classes'])
 def test_on_points_that_do_not_mix(space, classes, expected_labels):
-    labeling = KDTreeLabeling(r=2, bound=6)
-    assert np.array_equal(labeling.fit_predict(space, classes), expected_labels)
+    jad = JAD(r=2, bound=6)
+    assert np.array_equal(jad.fit_predict(space, classes), expected_labels)
 
 
 @pytest.mark.parametrize("space, classes, expected_labels", [
@@ -62,7 +64,6 @@ def test_on_points_that_do_not_mix(space, classes, expected_labels):
     (np.array([[7, 7], [4, 4], [5, 5]]), np.array([0, 0, 1]), [0, -1, -1])
 ], ids=['two_mixed_points', 'one_point_from_class_and_two_mixed_points'])
 def test_on_mixed_points(space, classes, expected_labels):
-    labeling = KDTreeLabeling(r=2, bound=0)
-    assert np.array_equal(labeling.fit_predict(space, classes), expected_labels)
-
+    jad = JAD(r=2, bound=0)
+    assert np.array_equal(jad.fit_predict(space, classes), expected_labels)
 
