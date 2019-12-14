152d8d97f364d7ec0765e679b010244cb0fb0f13
Maksim Zayakin <rasetsu@list.ru>
2019-12-15 00:13:53 +0500

    removed requirements; added setup.py; added src folder

--- tests/test_meepo.py
+++ tests/test_meepo.py
@@ -1,20 +1,20 @@
 import pytest
 import numpy as np
-from meepo import Meepo
+from src.meepo import meepo
 
 
 def test_fail_on_negative_r():
     with pytest.raises(ValueError):
-        Meepo(eps=-3, min_samples=2)
+        meepo.Meepo(eps=-3, min_samples=2)
 
 
 def test_fail_on_negative_bound():
     with pytest.raises(ValueError):
-        Meepo(eps=3, min_samples=-2)
+        meepo.Meepo(eps=3, min_samples=-2)
 
 
 def test_fail_on_empty_space():
-    jad = Meepo(eps=1, min_samples=2)
+    jad = meepo.Meepo(eps=1, min_samples=2)
     space = np.array([])
     classes = np.array([])
     with pytest.raises(ValueError):
@@ -22,7 +22,7 @@ def test_fail_on_empty_space():
 
 
 def test_fail_on_space_with_nans():
-    jad = Meepo(eps=1, min_samples=2)
+    jad = meepo.Meepo(eps=1, min_samples=2)
     space = np.array([[np.inf, np.nan], [5, 4]])
     classes = np.array([0, 1])
     with pytest.raises(ValueError):
@@ -30,7 +30,7 @@ def test_fail_on_space_with_nans():
 
 
 def test_fail_on_empty_classes():
-    jad = Meepo(eps=1, min_samples=2)
+    jad = meepo.Meepo(eps=1, min_samples=2)
     space = np.array([[5, 5]])
     classes = np.array([])
     with pytest.raises(ValueError):
@@ -38,7 +38,7 @@ def test_fail_on_empty_classes():
 
 
 def test_fail_on_classes_with_nans():
-    jad = Meepo(eps=1, min_samples=2)
+    jad = meepo.Meepo(eps=1, min_samples=2)
     space = np.array([5, 3])
     classes = np.array([np.nan])
     with pytest.raises(ValueError):
@@ -55,7 +55,7 @@ def test_fail_on_classes_with_nans():
 ], ids=['one_point', 'two_points_from_one_class', 'four_points_from_two_classes', 'six_points_from_three_classes',
         '1d_date_from_two_classes'])
 def test_on_points_that_do_not_mix(space, classes, expected_labels):
-    jad = Meepo(eps=2, min_samples=6)
+    jad = meepo.Meepo(eps=2, min_samples=6)
     assert np.array_equal(jad.fit_predict(space, classes), expected_labels)
 
 
@@ -64,6 +64,6 @@ def test_on_points_that_do_not_mix(space, classes, expected_labels):
     (np.array([[7, 7], [4, 4], [5, 5]]), np.array([0, 0, 1]), [0, -1, -1])
 ], ids=['two_mixed_points', 'one_point_from_class_and_two_mixed_points'])
 def test_on_mixed_points(space, classes, expected_labels):
-    jad = Meepo(eps=2, min_samples=0)
+    jad = meepo.Meepo(eps=2, min_samples=0)
     assert np.array_equal(jad.fit_predict(space, classes), expected_labels)
 
